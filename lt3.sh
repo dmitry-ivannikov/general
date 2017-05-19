95062ab9337d19d61b5ad851929217fb69e81d0b
qateam2 <ivannikov.dmitry.v@gmail.com>
2017-05-19 13:59:42 +0300

    new files4

--- lt3.sh
+++ lt3.sh
@@ -1,47 +1,35 @@
 #!/bin/bash
-
-inkrementDay=$DAYS #либо так
-#inkrementDay=-2 
+#incrementDay=$DAYS #Р»РёР±Рѕ С‚Р°Рє
+incrementDay=-2 
 
 #date of acces log file and name of test folder 
-accessLogDate="$(date -d "$inkrementDay day" +"%Y%m%d")"           
-
+accessLogDate="$(date -d "$incrementDay day" +"%Y%m%d")" 
+          
 #name of reportFolder
-reportFolderNname="$(date +%Y-%m-%d-%H-%M-%S)"                     
-
+reportFolderNname="$(date +%Y-%m-%d-%H-%M-%S)"  
+                   
 #dir of reportFolder
-reportFolderDir=/home/qateam/Reports/publo-mel/                    
-
-#project filder
-dirOfProjectTest=/home/qateam/melloadtest                          
-
+reportFolderDir=/home/qateam/Reports/publo-mel/   
+                 
 #fullname of accesslog file
-nameOfAccessLogFile=pablo-mel-main_$accessLogDate.access.log       
+nameOfAccessLogFile=pablo-mel-main_$accessLogDate.access.log
+
+#fullname of accesslog file2
+nameOfAccessLogFile2=pablo-mel-main_$accessLogDate.access2.log
 
 #fullname of accesslog archive
 nameOfAccessLogArchive=$nameOfAccessLogFile.gz 
 
 #directory of accesslog archive
-dirOfAccessLogFrom=/mnt/logs/nginx/nginx/$nameOfAccessLogArchive   
-
-#dir of test
-dirOfLoadTest=$dirOfProjectTest/$accessLogDate                     
-
-#url for download conf.ini
-urlofIniFile=https://raw.githubusercontent.com/qateam2/general/master/conf.ini 
-
-
+dirOfAccessLogFrom=/mnt/logs/nginx/nginx/$nameOfAccessLogArchive
+   
+##dir of test
+dirOfLoadTest=/home/qateam/workspace/TestLoadLan/general-master  
+ 
 copy_accesslog() { 
 mkdir -p "$dirOfLoadTest"
 cp $dirOfAccessLogFrom $dirOfLoadTest
 gunzip -f "$dirOfLoadTest/$nameOfAccessLogArchive"
-}
-
-
-
-delete_lines() { 
-sed -i '/HTTP\/1.[0,1]\" [4,5]/d' "$dirOfLoadTest/$nameOfAccessLogFile"
-sed -i '/300\] \"[HEAD,POST]/d' "$dirOfLoadTest/$nameOfAccessLogFile"
 }
 
 
@@ -51,29 +39,15 @@ find $dirOfLoadTest -iname '*.html' -and -type f -mmin -1 -exec cp '{}' $reportF
 }
 
 
-deletefiles(){
-rm -rf $dirOfProjectTest/$accessLogDate
-}
-
 #copy access.log file from publo dir
 copy_accesslog  
 
-# download conf.ini file for yandex-tank  
-curl -o "$dirOfLoadTest/conf.ini" $urlofIniFile
-
-# Delete requests with 400 & 500 code
-delete_lines  
+# Delete requests with 400 & 500 code 
+awk '$9 < 400' "$dirOfLoadTest/$nameOfAccessLogFile" | grep GET > "$dirOfLoadTest/$nameOfAccessLogFile2"
 
 # mixing queries
-shuf "$dirOfLoadTest/$nameOfAccessLogFile" --output="$dirOfLoadTest/$nameOfAccessLogFile"
-
-# now fullname if accessLogFile is access.log its for cofig ini file
-mv -f "$dirOfLoadTest/$nameOfAccessLogFile" "$dirOfLoadTest/access.log"
-
+shuf "$dirOfLoadTest/$nameOfAccessLogFile2" --output="$dirOfLoadTest/access.log"
 # start load test
-cd $dirOfLoadTest
-yandex-tank -c "conf.ini"
-
+##yandex-tank -c "conf.ini"
 #copy results from test dir to publoc results dir
-copyresults
-#deletefiles
+##copyresults
